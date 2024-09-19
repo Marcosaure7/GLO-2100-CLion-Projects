fe36a05d97dfecce5c91ba8105201650e189a8a4
Marcosaure7 <magou277@ulaval.ca>
2024-09-19 19:50:30 -0400

    TD1 Configuration machine virtuelle

--- TD1/Partie3/cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mag20/OneDrive - Université Laval/4e session/Algorithmes et strustures de données/Labs/TD1/Partie3/lib/googletest/googletest
-# Build directory: C:/Users/mag20/OneDrive - Université Laval/4e session/Algorithmes et strustures de données/Labs/TD1/Partie3/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- TD1/Partie3/cmake-build-debug/tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mag20/OneDrive - Université Laval/4e session/Algorithmes et strustures de données/Labs/TD1/Partie3/tests/lib/googletest
-# Build directory: C:/Users/mag20/OneDrive - Université Laval/4e session/Algorithmes et strustures de données/Labs/TD1/Partie3/cmake-build-debug/tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
